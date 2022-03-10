9473c9ce95ad435414249e60733f5f23b72d94cb
sikandarali307 <sikandar.ali307@gmail.com>
2022-03-10 16:01:22 +0500

    Initial Commit

---
+++ SwiftuiGrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftuiGrid
+//
+//  Created by Mac on 10/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftuiGrid/SwiftuiGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiGridApp.swift
+//  SwiftuiGrid
+//
+//  Created by Mac on 10/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
